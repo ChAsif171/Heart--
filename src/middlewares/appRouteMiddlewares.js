28d89aa1825ff560ac84e7d2c7d18913f625e41c
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2023-11-01 16:30:36 +0500

    Update appRouteMiddlewares.js

--- src/middlewares/appRouteMiddlewares.js
+++ src/middlewares/appRouteMiddlewares.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import mongoSanitize from "express-mongo-sanitize"; // to prevent sql query attack
-import xss from "xss-clean"; // to prevent from injecting scripts like html scripts (alert etc)
+//import xss from "xss-clean"; // to prevent from injecting scripts like html scripts (alert etc)
 
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 app.use(mongoSanitize()); // must come after express.json()
-app.use(xss());
+//app.use(xss());
 app.use(morgan(":method :url :status - :response-time ms - :res[content-length]"));
 
 export default app;
