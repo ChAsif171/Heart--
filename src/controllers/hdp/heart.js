5adc4bcb31f00e2d006a7e5a200e4f5d6c18e4a7
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2023-11-01 16:28:36 +0500

    Update heart.js

--- src/controllers/hdp/heart.js
+++ src/controllers/hdp/heart.js
@@ -8,12 +8,14 @@ import result from "../../models/result.js";
 const heart = async (req, res, next) => {
     try {
         let newUser;
-        const { user } = req;
+        const user = { email: "chasiflyh@gmail.com" };
+        console.log(user);
         var { age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal } = req.body;
         const apiBody = {
             email: user.email,
             age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal
         };
+        console.log(apiBody);
         const result1 = await hearts.findOne({ email: user.email });
         if (result1) {
             await hearts.updateMany({ email: user.email }, apiBody);
@@ -36,20 +38,32 @@ const heart = async (req, res, next) => {
         slope = req.body.slope || 1;
         ca = req.body.ca || 0;
         thal = req.body.thal || 1;
+
+        // age = 50;
+        // sex = 1;
+        // cp = 4;
+        // trestbps = 280;
+        // chol = 200;
+        // fbs = 0;
+        // restecg = 3;
+        // thalach = 180;
+        // exang = 3;
+        // oldpeak = 0.7;
+        // slope = 2;
+        // ca = 2;
+        // thal = 1;
+
         const pre_pocessing = { age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal };
 
         const results = await predict(pre_pocessing);
         console.log(results);
         const apiResult = {
-            email: user.email,
-            age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal,
-            results,
+            email: user.email, age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal,results
         };
-
         newUser = new result(apiResult);
         await newUser.save();
 
-        return sendSuccessResponse(res, 200, true, " Update User Records", "Heart API", newUser);
+        return sendSuccessResponse(res, 200, true, " Update User Records", "Heart API", apiResult);
     } catch (error) {
         print("error in heart", error);
         next(error);
