0360a347d3eef3b4829a48c5d28f2983e2dc36a0
Choudhary Asif <109628034+ChAsif171@users.noreply.github.com>
2023-11-01 16:28:45 +0500

    Update hdp.js

--- src/routes/hdp.js
+++ src/routes/hdp.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 import Users from "../models/users.js";
 
 const router = express.Router();
-router.post("/disease",authMiddleware(Users),heart);
+router.post("/disease",heart);
 router.post("/nn",predection);
 
 export default router;
